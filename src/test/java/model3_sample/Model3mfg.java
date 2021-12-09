10a9ec07ebfcd17293163b5957381f90a2490772
shilp <shilp@LAPTOP-RFKFALFD>
2021-12-09 15:48:52 +0530

    commit1

---
+++ src/test/java/model3_sample/Model3mfg.java
@@ -0,0 +1,13 @@
+package model3_sample;
+
+import org.testng.annotations.Test;
+
+public class Model3mfg {
+	@Test
+	public void model3() {
+		System.out.println("model1");
+		System.out.println("model2");
+
+	}
+
+}
